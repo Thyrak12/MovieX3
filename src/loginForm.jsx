e8c994651944244a10f451a49c85984586f16e63
youdy <youdy017@gmail.com>
2025-02-28 22:54:23 +0700

    login page done form

--- src/loginForm.jsx
+++ src/loginForm.jsx
@@ -1,12 +1,12 @@
-
+import './login.css'
 function LoginForm() {
   return (
-    <div>
-      <form>
-        <h1>Login</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+    <div className="login-container">
+      <form className="login-form">
+        <div id="SignInToMovieX3">Sign in to MovieX3</div>
+        <input type="text" placeholder="Username" /> <br />
+        <input type="password" placeholder="Password" /><br />
+        <button id="loginbutton" type="submit">Login</button>
       </form>
     </div>
   );
