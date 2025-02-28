b526de51fcfd2f69f7dda0bb8be7bb8ec4a1a423
Povpanha28 <sengkim28140@gmail.com>
2025-02-28 11:48:50 +0700

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css';
-import Home from './Home';
-import Header from './Header';
-import Footer from './Footer';
-import { Routes, Route } from 'react-router-dom';
-import Movie from './Movie';
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
+
   return (
-    <div>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Loki" element={<Movie />} />
-      </Routes>
+      <Home />
       <Footer />
-    </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/MovieX3">
-<<<<<<< Updated upstream
-      <App />
-=======
-      <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="*" element={<login_Page/>} />
-      </Routes>
->>>>>>> Stashed changes
-    </BrowserRouter>
+     <App />
   </StrictMode>
 );
