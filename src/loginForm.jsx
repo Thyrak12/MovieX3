6a22716924637de8396afb81c7ce6d0e8ccd3e36
youdy <youdy017@gmail.com>
2025-03-01 14:31:46 +0700

    update login page (more decorection)

--- src/loginForm.jsx
+++ src/loginForm.jsx
@@ -1,4 +1,6 @@
 import './login.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function LoginForm() {
   return (
     <div className="login-container">
@@ -8,6 +10,11 @@ function LoginForm() {
         <input type="password" placeholder="Password" /><br />
         <button id="loginbutton" type="submit">Login</button>
       </form>
+      <div className='createAccContainer'>
+        <div className='NewtoMovieX3'>New to MovieX3?</div> <br />
+        <div className='createAcc'>Create an account</div>
+      </div>
+      
     </div>
   );
 }
