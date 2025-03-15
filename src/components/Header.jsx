0e9e89bde97f84d660ce02d7c8622955acc79a73
RBunna <bunnasovethroath@gmail.com>
2025-03-15 08:50:54 +0700

    Append README 	- Add installation guide 	- Accepting incoming changes in main.jsx and Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,15 +46,6 @@ export default function Header() {
     display: (location.pathname === "/" || isScrolled) ? "block" : "none", // Show on Home page or when scrolled
   };
 
-<<<<<<< HEAD
-  if (window.scrollY > 50) {
-    setIsScrolled(true);
-    
-  } else {
-    setIsScrolled(false);
-  }
-};
-=======
   return (
     <>
       <Navbar expand="lg" className="navbar-dark" style={navbarStyle}>
@@ -67,7 +58,6 @@ export default function Header() {
             <Nav className="me-auto my-2 my-lg-0" navbarScroll>
               <Nav.Link href="#action1">Home</Nav.Link>
               <Nav.Link href="#action2">Latest</Nav.Link>
->>>>>>> be486d3a42ddee566fdd9e5bfd1870a1b60734fd
 
               <NavDropdown title="Categories" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -124,27 +114,6 @@ export default function Header() {
                   aria-label="Search"
                 />
 
-<<<<<<< HEAD
-    <ToastContainer position="top-end" className="p-3">
-      <Toast
-        show={showToast}
-        onClose={() => setShowToast(false)}
-        delay={3000}
-        autohide
-        bg="light"
-      >
-        <Toast.Header>
-          <i className="bi bi-bell-fill me-2 text-warning"></i>
-          <strong className="me-auto">Notifications</strong>
-          <small>Just now</small>
-        </Toast.Header>
-        <Toast.Body>You have new notifications!</Toast.Body>
-      </Toast>
-    </ToastContainer>
-  </>
-);
-
-=======
                 <Button
                   id="button"
                   style={{ outline: "none", border: "none" }}
@@ -177,4 +146,3 @@ export default function Header() {
     </>
   );
 }
->>>>>>> be486d3a42ddee566fdd9e5bfd1870a1b60734fd
