62c9e8ed623ed3ca7dc62e0a7c665d665d346fbd
youdy17 <youdy017@gmail.com>
2025-03-27 08:41:07 +0700

    clean repo tree

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-<<<<<<< HEAD
-=======
 // import './main.css';
->>>>>>> 884587cd9c19870652cf12f69da85b8bd6c60019
 
 
 createRoot(document.getElementById('root')).render(
