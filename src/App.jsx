1d0f04e0af03e2c2209c64e1a4d8e9fc4a1b7fd6
Povpanha28 <sengkim28140@gmail.com>
2025-03-28 13:48:52 +0700

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,17 @@ import MovieCard from './components/Card';
 function App() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/:name" element={<Movie />} />
         <Route path="/Latest-Movies" element={<DisplayByCategory category={"Latest Movies"} />} />
         <Route path="/Action-Movies" element={<DisplayByCategory category={"Action Movies"} />} />
         <Route path="/Dramas" element={<DisplayByCategory category={"Dramas"} />} />
         <Route path="/Sci-Fi" element={<DisplayByCategory category={"Sci-Fi"} />} />
-        Just add new routes below don't edit all
+        {/* Just add new routes below don't edit all */}
       </Routes>
-      <Footer /> */}
-      <Header />
-      <Search />
-      
+      <Footer />
     </>
   );
 }
